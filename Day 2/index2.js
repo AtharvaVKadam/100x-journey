f82b5a60a3163577c5855c193559f97c8d87e948
AtharvaVKadam <atharvak731@gmail.com>
2026-01-30 02:11:03 +0530

    Completed Authentication System with /me route

---
+++ Day 2/index2.js
@@ -0,0 +1,117 @@
+    const express = require("express");
+    const jwt = require("jsonwebtoken");
+    const jwtPassword = "123456";
+
+    const app = express();
+    app.use(express.json());
+
+    const ALL_USERS = [
+        {
+            username: "Yash@gmail.com",
+            password: "Yash@11",
+            name: "Yash"
+        },
+
+        {
+            username: "Ram@gmail.com",
+            password: "Ram@11",
+            name: "Ram"
+        },
+
+        {
+            username: "Soham@gmail.com",
+            password: "Soham@11",
+            name: "Soham"
+        }
+    ]
+
+    function userExists(username,password){
+
+        for( let i= 0; i<ALL_USERS.length; i++ ){
+            if ( ALL_USERS[i].username == username && ALL_USERS[i].password == password){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    app.post("/signin",function(req,res){
+        const username = req.body.username;
+        const password = req.body.password;
+        const validUser = userExists(username,password);
+
+        if(!validUser){
+
+            res.status(403).json({
+                mssg: "Invalid incredentials"
+            });
+            return;
+        }
+        var token = jwt.sign({username : username},jwtPassword);
+
+        res.json({
+                mssg:" You are signed in",
+                token
+            });
+        })  
+
+    app.get("/users", function(req,res){
+        const token = req.headers.authorization;
+
+        if(token){
+            try{
+                const decoded = jwt.verify(token,jwtPassword);
+            
+                res.json({
+                    mssg:"Welcome back "+decoded.username,
+                    users: ALL_USERS
+                })
+            }
+            catch(e){
+                res.status(403).json({
+                    mssg:"Token incorrect"
+                })
+            }
+        }
+        else{
+            res.status(403).json({
+                mssg: "No token sent"
+            })
+        }
+        
+    })
+
+    app.get("/me", function(req, res) {
+
+        const token = req.headers.authorization;
+
+        if (token) {
+            try {
+                const decoded = jwt.verify(token, jwtPassword);
+                const username = decoded.username; 
+
+                // CHALLENGE: Write a loop (or use .find) to get THIS user's data from ALL_USER
+                
+                    const foundUser = ALL_USERS.find(
+                        u => u.username == username
+                    );
+                    
+                if (foundUser) {
+                    res.json({
+                        username: foundUser.username,
+                        password: foundUser.password
+                    })
+                } else {
+                    res.status(403).json({ msg: "User not found" });
+                }
+
+            } catch(e) {
+                res.status(403).json({ msg: "Invalid token" });
+            }
+        } else {
+            res.status(403).json({ msg: "No token sent" });
+        }
+    });
+
+    app.listen(3001);
