9e6da09f00a3a1fee8211b3643cc6425a067f508
AtharvaVKadam <atharvak731@gmail.com>
2026-02-03 23:56:11 +0530

    Starting with React from Counter project

---
+++ Day 7/App.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+function App(){
+    const [count, setCount] = useState(0);
+
+    return (
+        <div>
+            <Button const count = {count} setCount = {setCount}></Button>
+        </div>
+    )
+}
+
+function Button(props){
+    function OnButtonClick() {
+        props.setCount(props.count + 1);
+    }
+    return(
+        <button onClick={OnButtonClick}>Counter {props.count}</button>
+    )
+}
+
+export default App;
