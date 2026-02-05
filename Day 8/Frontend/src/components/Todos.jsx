52cb66225e97ee7b319b45596aad5813386cb29d
AtharvaVKadam <atharvak731@gmail.com>
2026-02-05 16:41:04 +0530

    Added Mark as done button

--- Day 8/Frontend/src/components/Todos.jsx
+++ Day 8/Frontend/src/components/Todos.jsx
@@ -1,17 +1,25 @@
-export function Todos({ todos }) {
-    return (
-        <div>
-            {todos.map(function(todo) {
-                return (
-                    <div key={todo._id}> 
-                        <h1>{todo.title}</h1>
-                        <h2>{todo.description}</h2>
-                        <button>
-                            {todo.completed == true ? "Completed" : "Mark as complete"}
-                        </button>
-                    </div>
-                )
-            })}
-        </div>
-    )
+export function Todos({todos}) {
+    return <div>
+        {todos.map(function(todo) {
+            return <div key={todo._id}>
+                <h1>{todo.title}</h1>
+                <h2>{todo.description}</h2>
+                <button onClick={() => {
+                    fetch("http://localhost:3000/completed", {
+                        method: "PUT",
+                        body: JSON.stringify({
+                            id: todo._id
+                        }),
+                        headers: {
+                            "Content-type": "application/json"
+                        }
+                    })
+                        .then(async function(res) {
+                            const json = await res.json();
+                            alert("Marked as done!");
+                        })
+                }}>{todo.completed == true ? "Completed" : "Mark as Complete"}</button>
+            </div>
+        })}
+    </div>
 }
