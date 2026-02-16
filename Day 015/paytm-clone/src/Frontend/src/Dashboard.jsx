f49828275484e3811e37307dc01e8d28267085a7
AtharvaVKadam <atharvak731@gmail.com>
2026-02-16 22:57:39 +0530

    Some more changes

--- Day 015/paytm-clone/src/Frontend/src/Dashboard.jsx
+++ Day 015/paytm-clone/src/Frontend/src/Dashboard.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import api from "./api"; 
+import api from "../api"; 
 
 export const Dashboard = () => {
     const [balance, setBalance] = useState(0);
