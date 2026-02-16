77c403f19cd11a028c114fe0cbb463752af05da1
AtharvaVKadam <atharvak731@gmail.com>
2026-02-16 22:52:09 +0530

    Creating Frontend

--- Day 015/paytm-clone/src/Frontend/App.jsx
+++ Day 015/paytm-clone/src/Frontend/App.jsx
@@ -1,11 +1,18 @@
-function App(){
-    return(
-        <>
-            <h1>
-                Hello World!
-            </h1>
-        </>
-    )
-};
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Dashboard } from "./Dashboard";
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/dashboard" />} />
+        
+        <Route path="/dashboard" element={<Dashboard />} />
+        
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 export default App;
