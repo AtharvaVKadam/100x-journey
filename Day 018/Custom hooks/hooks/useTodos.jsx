42c17e438cb1fe69d2d17ebbd95ee7753b526e95
AtharvaVKadam <atharvak731@gmail.com>
2026-02-20 15:16:26 +0530

    Added a interval so that data refreshes every 10 sec and also clearTimeout so that App doesnt crash

--- Day 018/Custom hooks/hooks/useTodos.jsx
+++ Day 018/Custom hooks/hooks/useTodos.jsx
@@ -7,11 +7,24 @@ function useTodos(){
     const [loading,setLoading] = useState(true);
 
     useEffect(()=> {
-        axios.get("")
-        .then(res => {
-            setTodos(res.data.todos);
-            setLoading(false);
-        })
+        function fetchTodos(){
+            axios.get("")
+                .then(res => {
+                    setTodos(res.data.todos);
+                    setLoading(false);
+                })
+                .catch(err => {
+                    console.error("Error fetching todos:", err);
+                    setLoading(false);
+                });
+            }
+            fetchTodos();
+
+            const intervalId = setInterval( fetchTodos, 10000 );
+
+            return() => {
+                clearInterval(intervalId);
+            }
     }, [])
 
     return { todos, loading };
