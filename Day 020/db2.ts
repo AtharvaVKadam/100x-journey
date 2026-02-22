e8dbdd40d9157aa9fb082de0933099f8590a8f07
AtharvaVKadam <atharvak731@gmail.com>
2026-02-22 22:06:24 +0530

    implemented secure parameterized queries for user insertion and fetching

---
+++ Day 020/db2.ts
@@ -0,0 +1,29 @@
+
+async function manageUsers() {
+    try {
+        await client.connect();
+        console.log("🚀 Connected to PostgreSQL!");
+
+        const insertQuery = `
+            INSERT INTO users (username, email) 
+            VALUES ($1, $2) 
+            RETURNING id, username, email;
+        `;
+        const newUserValues = ['atharva_dev', 'atharva@meetai.com'];
+        
+        const insertRes = await client.query(insertQuery, newUserValues);
+        console.log("User Inserted:", insertRes.rows[0]);
+
+        const fetchQuery = `SELECT * FROM users WHERE username = $1;`;
+        const fetchRes = await client.query(fetchQuery, ['atharva_dev']);
+        
+        console.log("User Fetched from DB:", fetchRes.rows[0]);
+
+    } catch (error) {
+        console.error("Database operation failed:", error);
+    } finally {
+        await client.end(); 
+    }
+}
+
+manageUsers();
