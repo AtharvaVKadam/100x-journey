949b211426bafa10416980304b5acfe3a6550d5c
AtharvaVKadam <atharvak731@gmail.com>
2026-02-27 17:24:32 +0530

    implemented strict interface typing for object parameters in TypeScript

---
+++ Day 025/src/index.ts
@@ -0,0 +1,11 @@
+interface User {
+    name : string;
+    age : number
+}
+
+function sumOfAge( user1 : User, user2 : User) {
+    return user1.age + user2.age
+}
+
+const age = sumOfAge ({ name : 'John' , age : 20 }, { name : 'Ronnie' , age : 24 });
+console.log(age);
