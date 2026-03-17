8aec7a5e0283eea8fb036fef374f21c1e574152b
AtharvaVKadam <atharvak731@gmail.com>
2026-03-17 22:16:35 +0530

    MInor updates

--- Day 31/server.ts
+++ Day 31/server.ts
@@ -3,17 +3,14 @@ import cors from 'cors';
 import authRoutes from './authRoutes';
 import uploadRoutes from './uploadRoutes';
 
-app.use('/api/upload', uploadRoutes);
-
 const app = express();
 const PORT = 5000; 
-
 
 app.use(cors()); 
 app.use(express.json()); 
 
-
 app.use('/api', authRoutes); 
+app.use('/api/upload', uploadRoutes);
 
 app.listen(PORT, () => {
     console.log(`🚀 MeetAI Backend running securely on http://localhost:${PORT}`);
